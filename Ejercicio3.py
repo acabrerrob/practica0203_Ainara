671983799ff90dbd5ebb02f215fbc1df2ac6459e
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-02 23:36:21 +0100

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,20 +1,13 @@
-# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas,
-# Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha 
-# sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura>
-# has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota>
-# cada una de las correspondientes notas introducidas por el usuario.
 subjectsList = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
 grades = []
+
 for subject in subjectsList:
-    grade = float (input(f'Introduzca la nota de {subject}'))
+    grade = float (input(f'Introduzca la nota de {subject} '))
 
     grades.append(grade)
 
+count = 0
 
 for grade in grades :
-        print (f'En {subjectsList} has sacado {grade}')
-# print('En ', subjectsList[0], 'has sacado ', grades[0])
-# print('En ', subjectsList[1], 'has sacado ', grades[1])
-# print('En ', subjectsList[2], 'has sacado ', grades[2])
-# print('En ', subjectsList[3], 'has sacado ', grades[3])
-# print('En ', subjectsList[4], 'has sacado ', grades[4])
+        print (f'En {subjectsList[count]} has sacado {grade}')
+        count +=1
