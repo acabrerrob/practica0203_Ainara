93415ce55b42fb4389b4871d28ea00b9dc77d472
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-02 15:54:31 +0100

    .

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -5,15 +5,16 @@
 # cada una de las correspondientes notas introducidas por el usuario.
 subjectsList = ['Matemáticas', 'Física', 'Química', 'Historia', 'Lengua']
 grades = []
-for x in subjectsList:
-    grade = float (input('Introduzca la nota de '))
+for subject in subjectsList:
+    grade = float (input(f'Introduzca la nota de {subject}'))
 
     grades.append(grade)
 
-print(len(subjectsList))
 
-print('En ', subjectsList[0], 'has sacado ', grades[0])
-print('En ', subjectsList[1], 'has sacado ', grades[1])
-print('En ', subjectsList[2], 'has sacado ', grades[2])
-print('En ', subjectsList[3], 'has sacado ', grades[3])
-print('En ', subjectsList[4], 'has sacado ', grades[4])
+for grade in grades :
+        print (f'En {subjectsList} has sacado {grade}')
+# print('En ', subjectsList[0], 'has sacado ', grades[0])
+# print('En ', subjectsList[1], 'has sacado ', grades[1])
+# print('En ', subjectsList[2], 'has sacado ', grades[2])
+# print('En ', subjectsList[3], 'has sacado ', grades[3])
+# print('En ', subjectsList[4], 'has sacado ', grades[4])
